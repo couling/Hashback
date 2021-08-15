33abdefea1473aa6a576477fe1a65a6769c28160
Philip Couling <couling@gmail.com>
2021-08-15 23:53:49 +0100

    Git versioning

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup
-from setuptools.config import read_configuration
-
-conf_dict = read_configuration("setup.cfg")
 
 setup(
-    **conf_dict['metadata'],
-    **conf_dict['options'],
+    version_config=True,
+    setup_requires=['setuptools-git-versioning'],
 )
